728719e0974b696bbe8ec5886861b89ba1c710fb
Mridhul <mridhul67@gmail.com>
2024-06-16 23:57:35 +0530

    Import and run function to add tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./styles/style.css";
 import "./styles/modern-normalize.css";
+import { addtask } from "./barrel";
+
+const addTaskBtn = document.querySelector(".add-task");
+
+addTaskBtn.addEventListener("click", addtask);
