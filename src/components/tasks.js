ba98c698ee167190c2e005641a6fa517ddb543d4
Mridhul <mridhul67@gmail.com>
2024-07-29 01:13:33 +0530

    Folder not needed anymore

--- src/components/tasks.js
+++
@@ -1,9 +0,0 @@
-export { addtask };
-
-const tasksContainer = document.querySelector(".tasks");
-
-function addtask() {
-  const newTask = document.createElement("div");
-  newTask.classList.toggle("task");
-  tasksContainer.appendChild(newTask);
-}
