7c98da1531975f6583f0988f3673c6f09515862c
Mridhul <mridhul67@gmail.com>
2024-07-30 23:51:36 +0530

    Run function to add tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./styles/style.css";
 import "./styles/modern-normalize.css";
-import { renderTasks } from "./barrel";
+import { renderTasks, addTask } from "./barrel";
 
 renderTasks();
+
+const addBtn = document.querySelector(".add-task");
+
+addBtn.addEventListener("click", addTask.bind(null, "Sample"));
+addBtn.addEventListener("click", renderTasks);
