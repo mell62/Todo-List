685f3f877cb559b79016faf06d876db3f960aa62
Mridhul <mridhul67@gmail.com>
2024-08-01 22:28:39 +0530

    Write function to delete specific task from tasks array

--- src/components/user-interface.js
+++ src/components/user-interface.js
@@ -1,12 +1,12 @@
-export { renderTasks };
-import { taskLibrary } from "../barrel";
+export { renderTasks, deleteTask };
+import { taskLibrary, removeTask } from "../barrel";
 
 const tasksContainer = document.querySelector(".tasks");
 
 function createDeleteBtn() {
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "🗑";
-  deleteBtn.classList.toggle("delete-task");
+  deleteBtn.classList.toggle("delete-btn");
   return deleteBtn;
 }
 
@@ -26,4 +26,10 @@ function cleanTasks() {
   tasks.forEach((task) => {
     tasksContainer.removeChild(task);
   });
+}
+
+function deleteTask(deleteBtn) {
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+  const deleteBtnIndex = Array.prototype.indexOf.call(deleteBtns, deleteBtn);
+  removeTask(deleteBtnIndex);
 }
