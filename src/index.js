1c04de64062fd69b950d92a7ba8d68bf3ddefc2c
Mridhul <mridhul67@gmail.com>
2024-08-01 22:30:08 +0530

    Import and run function to delete task upon clicking delete button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import "./styles/style.css";
 import "./styles/modern-normalize.css";
-import { renderTasks, addTask } from "./barrel";
+import { renderTasks, addTask, deleteTask } from "./barrel";
 
 renderTasks();
 
+const tasksContainer = document.querySelector(".tasks");
 const addBtn = document.querySelector(".add-task");
 
+// Add tasks
 addBtn.addEventListener("click", addTask.bind(null, "Sample"));
 addBtn.addEventListener("click", renderTasks);
+
+//Delete tasks
+tasksContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    deleteTask(event.target);
+    renderTasks();
+  }
+});
