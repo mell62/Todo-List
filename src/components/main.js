f20fd13e7db1db5c121bee6784adcb32937d575e
Mridhul <mridhul67@gmail.com>
2024-08-03 15:07:45 +0530

    Write function to rename and save task to taskLibrary array

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,15 @@
-import { removeTask } from "../barrel";
+import { removeTask, taskLibrary } from "../barrel";
 
-export { deleteTask };
+export { deleteTask, saveTask };
 
 function deleteTask(deleteBtn) {
   const deleteBtns = document.querySelectorAll(".delete-btn");
   const deleteBtnIndex = Array.prototype.indexOf.call(deleteBtns, deleteBtn);
   removeTask(deleteBtnIndex);
+}
+
+function saveTask(doneBtn, newTitle) {
+  const doneBtns = document.querySelectorAll(".done-btn");
+  const doneBtnIndex = Array.prototype.indexOf.call(doneBtns, doneBtn);
+  taskLibrary[doneBtnIndex].taskTitle = newTitle;
 }
