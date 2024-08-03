03d8f75a66f991b2272d63f19d346fd7d0f9289a
Mridhul <mridhul67@gmail.com>
2024-08-03 15:09:15 +0530

    Run function to rename task by clicking Done button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/style.css";
 import "./styles/modern-normalize.css";
 import { renderTasks, addTask, deleteTask } from "./barrel";
+import { saveTask } from "./components/main";
 
 renderTasks();
 
@@ -8,13 +9,21 @@ const tasksContainer = document.querySelector(".tasks");
 const addBtn = document.querySelector(".add-task");
 
 // Add tasks
-addBtn.addEventListener("click", addTask.bind(null, "Sample"));
+addBtn.addEventListener("click", addTask.bind(null, "New Task"));
 addBtn.addEventListener("click", renderTasks);
 
 //Delete tasks
 tasksContainer.addEventListener("click", (event) => {
   if (event.target.classList.contains("delete-btn")) {
     deleteTask(event.target);
+    renderTasks();
+  }
+});
+
+//Rename tasks
+tasksContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("done-btn")) {
+    saveTask(event.target, event.target.nextSibling.value);
     renderTasks();
   }
 });
